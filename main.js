5e812be3dfc04320f7226da9baa4518f7c2adb2f
resterle <raphael@esterle.org>
2020-09-17 22:58:39 +0200

    links

--- main.js
+++ main.js
@@ -21,10 +21,6 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 }
 
-function doSomething(){
-  console.log('something')
-}
-
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
--- renderer.js
+++ renderer.js
@@ -1,62 +1,55 @@
-// include the ipc module to communicate with main process.
-const md = require('markdown-it')({linkify: true});
-const md_emoji = require('markdown-it-emoji');
-const md_task_lists = require('markdown-it-task-lists');
-const md_prism = require('markdown-it-prism');
-const md_mila = require('markdown-it-link-attributes')
-md
-    .use(md_emoji)
-    .use(md_task_lists)
-    .use(md_prism, {plugins: ['highlight-keywords', 'show-language']})
-    .use(md_mila, {
-        pattern: /^https:/,
-        attrs: {
-            target: '_blank',
-            rel: 'noopener'
-        }});
+const baseDir = '/home/raphael/.notes'
+const md = require('markdown-it')()
+    .use(require('markdown-it-emoji'))
+    .use(require('markdown-it-task-lists'))
+    .use(require('markdown-it-prism'), {plugins: ['highlight-keywords', 'show-language']})
+    .use(require('markdown-it-anchor'))
+    .use(require('markdown-it-mark'))
+    .use(require('markdown-it-meta'))
+    .use(require('markdown-it-wikilinks')({ uriSuffix: '', relativeBaseURL: '#', baseURL: `${baseDir}/` }));
 
 const fs = require('fs')
 const $ = require('jquery') 
 const shell = require('electron').shell;
 
-const baseDir = '/home/raphael/.notes'
+
 const mdRegEx = /.*\.md$/;
 let currentFile = 'test.md';
 let notes = [];
 
-loadAndDisplayHtml(currentFile);
 updateFileList();
 
 fs.watch('/home/raphael/.notes', (eventType, filename) => {
     if (filename && filename === currentFile && eventType === 'change') {
-        loadAndDisplayHtml(currentFile)
+        loadAndRender(currentFile)
     }
 });
 
 function setCurrentFile(current){
+    $('.title').text(nameFromFile(current));
     currentFile = current;
-    createNoteList();
+    loadAndRender(current);
+    createNoteList(current);
 }
 
 function noteClickedHandler(note){
     return () => {
         setCurrentFile(note);
-        loadAndDisplayHtml(note);
     }
 }
 
-function createNoteList(){
+function createNoteList(selectedNote=null){
     const elements = [];
     for(let note of notes.sort()){
-        elements.push(createNoteNavElement(note, currentFile));
+        elements.push(createNoteListElement(note, selectedNote));
     }
     const list = $('#note-list');
     list.html('');
     list.append(elements);
 }
 
-function createNoteNavElement(note){
-    const name = note.substring(0, note.length-3);
+function createNoteListElement(note){
+    const name = nameFromFile(note);
     const element = $(`<span class="nav-group-item"></span>`);
     const textElement = $(note === currentFile ? '<b></b>' : '<a></a>').text(name);
     element.append($('<span class="icon icon-doc-text"></span>'), textElement);
@@ -64,18 +57,22 @@ function createNoteNavElement(note){
     return element;
 }
 
-function updateFileList(){
+function nameFromFile(file){
+    return file.substring(0, file.length-3);
+}
+
+function updateFileList(selectedNote){
     fs.readdir(baseDir, (err, files) => {
         files.forEach(file => {
             if(mdRegEx.test(file)){
                 notes.push(file);
             }
         });
-        createNoteList();
+        createNoteList(selectedNote);
     });
 }
 
-function loadAndDisplayHtml(filename) {  
+function loadAndRender(filename) {  
     const absolutePath = `${baseDir}/${filename}`;
     //Check if file exists
     if(fs.existsSync(absolutePath)) {
@@ -90,4 +87,10 @@ function loadAndDisplayHtml(filename) {
 $(document).on('click', 'a[href^="https"]', function(event) {
     event.preventDefault();
     shell.openExternal(this.href);
+});
+
+$(document).on('click', `a[href^="${baseDir}"]`, function(event) {
+    event.preventDefault();
+    note = event.currentTarget.pathname.substr(baseDir.length+1);
+    noteClickedHandler(`${note}.md`)();
 });
